8abcb9fb573117f6c0625d53541f173d71286429
Smokey-444 <85306561+Smokey-444@users.noreply.github.com>
2021-07-01 18:53:42 +0200

    7

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ bot.on('ready', () => {
     bot.user.setPresence({
         activity: {
             name: 't!play',
-            type: 'watch',
+            type: 'WATCHING',
             url: 'https://www.youtube.com/watch?v=LYOrjkdbMDQ'
         }
     })
